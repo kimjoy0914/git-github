9f8ec2e3d9a6a24701dfea8066d389a22324f88c
kimjoy0914 <kimjoy0914@gmail.com>
2025-04-01 07:49:07 +0900

    version4

--- basic-syntax/1.basic-command.sh
+++ basic-syntax/1.basic-command.sh
@@ -1,2 +1,8 @@
 # git add .은 모든 변경(추가)사항을 staging area 이동
 git add .
+
+# git commit은 staging area의 변경사항을 확정짓고 commitID 생성
+git commit -m "first commit"
+
+# 원격 repo에 main브랜치 변경사항(commitID)을 업로드 
+git push origin main
