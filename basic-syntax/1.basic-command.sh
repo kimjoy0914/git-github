0bbc2f42d01b692896dbdb9e3abd4ae29f22210c
kimjoy0914 <kimjoy0914@gmail.com>
2025-04-06 04:01:47 +0900

    취소사항명령어정리

--- basic-syntax/1.basic-command.sh
+++ basic-syntax/1.basic-command.sh
@@ -46,3 +46,14 @@ git pull origin 브랜치명
 
 # 원격에 변경 사항을 local에 가져오되 병합은 하지 않은 것
 git fetch origin main
+
+# git 작업 취소
+# commit 이후의 취소
+git reset head~1
+git reset head^
+
+# staging area로 까지만 취소
+git reset --soft head~1
+
+# push 이후이 취소 -> commit 메시지 작성 vi화면으로 이동 -> :wq엔터
+git revert 기존의커밋IDㄹ
